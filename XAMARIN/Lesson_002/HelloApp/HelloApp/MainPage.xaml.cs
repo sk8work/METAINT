6961ba110e5c67f6ccdfbb5d0b67db4f3f26c4f3
sk8work <eshikhantsev@gmail.com>
2021-04-25 09:31:04 +0300

    Completed METAINT_Xamarin_lesson_002

---
+++ XAMARIN/Lesson_002/HelloApp/HelloApp/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HelloApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Press_Clicked(object sender, EventArgs e)
+        {
+            Press.Text = "I've been PRESSED!!";
+        }
+    }
+}
---
+++ XAMARIN/Lesson_002/HelloApp/HelloApp/Page1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace HelloApp
+{
+    public class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                  new Label { Text = "Hello from page1" }
+                }
+            };
+        }
+    }
+}
