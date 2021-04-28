3a0c1b606170caad384ba24833a7774935b2e828
sk8work <eshikhantsev@gmail.com>
2021-04-28 17:48:57 +0300

    METAINT_Xamarin_Lesson_004_DateTime not completed

---
+++ XAMARIN/Lesson_004/Date_Time/Date_Time/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Date_Time
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void datePicker_DateSelected(object sender, DateChangedEventArgs e)
+        {
+            if(label != null)
+            {
+                label.Text = "Вы выбрали " + e.NewDate.ToString("dd/MM/yyyy");
+            }
+        }
+    }
+}
