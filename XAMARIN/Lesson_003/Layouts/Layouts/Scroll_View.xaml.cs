bb8ab5887a88c9aef720d903437902da8066937e
sk8work <eshikhantsev@gmail.com>
2021-04-28 14:20:51 +0300

    METAINT_Xamarin_Lesson_004 completed

---
+++ XAMARIN/Lesson_003/Layouts/Layouts/Scroll_View.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Layouts
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Scroll_View : ContentPage
+    {
+        public Scroll_View()
+        {
+            InitializeComponent();
+        }
+    }
+}
